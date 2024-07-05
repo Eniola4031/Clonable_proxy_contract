909ec82f0277529a0bd92c2c9b19037436e133e4
Eniola4031 <agboola.eniola4031@gmail.com>
2024-07-05 12:41:07 +0100

    erc20

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
-import "../src/proxyerc20.sol";
+import "../src/Erc20proxyclone.sol";
+import "../src/Proxyclone.sol";
 
 contract TokenFactoryTest is Test {
     ERC20 public masterContract;
